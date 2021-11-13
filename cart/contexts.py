72103f6588cedcebc114cdbf44a6a2c5ca666b98
MarcJPH <marchuntzinger@hotmail.com>
2021-11-13 11:00:54 +0000

    Updated context processor to include product items

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,14 +1,26 @@
-
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 def cart_contents(request):
     """
     Any items added to the cart will be available to all templates and apps
     """
 
-
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
 
     context = {
         'cart_items': cart_items,
