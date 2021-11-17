79bf38a20a4a99011f032a5eff32bef680127d58
MarcJPH <marchuntzinger@hotmail.com>
2021-11-17 21:20:11 +0000

    Added toast messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -14,6 +17,7 @@ def add_to_cart(request, item_id):
     Add a quantity of the product to the cart
     """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -22,6 +26,20 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to the cart.')
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def remove_from_cart(request, item_id):
+    """Remove the item from the shopping cart"""
+
+    try:
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
