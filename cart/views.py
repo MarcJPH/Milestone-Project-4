92780c23a26ac04fe152efa3c9d37c544a94455a
MarcJPH <marchuntzinger@hotmail.com>
2021-11-18 19:40:56 +0000

    Added checkout app and models

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -17,7 +17,7 @@ def add_to_cart(request, item_id):
     Add a quantity of the product to the cart
     """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -37,9 +37,11 @@ def remove_from_cart(request, item_id):
 
     try:
         cart.pop(item_id)
+        messages.success(request, f'Removed {product.name} from the cart.')
 
         request.session['cart'] = cart
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}.')
         return HttpResponse(status=500)
