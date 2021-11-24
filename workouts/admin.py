504a7e8adb79805677294bce6c8df0f554390759
MarcJPH <marchuntzinger@hotmail.com>
2021-11-24 00:02:43 +0000

    Created workouts app, view, fixtures and model

---
+++ workouts/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Workouts, Category
+
+class WorkoutsAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'duration',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Workouts, WorkoutsAdmin)
+admin.site.register(Category, CategoryAdmin)
---
+++ workouts/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Workouts
+# Create your views here.
+
+def workouts(request):
+    """
+    A view to return all workouts available.
+    """
+
+    workouts = Workouts.objects.all()
+
+    context = {
+        'workouts': workouts,
+    }
+
+    return render(request, 'workouts/workouts.html', context)
+
+
