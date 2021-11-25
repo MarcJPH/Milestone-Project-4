37511271afb2d1e6020780ba0433089b6d83b948
MarcJPH <marchuntzinger@hotmail.com>
2021-11-25 20:22:25 +0000

    Added message on dropdowns if user not registered

--- workouts/views.py
+++ workouts/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Workouts
+
 # Create your views here.
 
 def workouts(request):
     """
     A view to return all workouts available.
     """
-
+    
     workouts = Workouts.objects.all()
 
     context = {
@@ -14,5 +15,3 @@ def workouts(request):
     }
 
     return render(request, 'workouts/workouts.html', context)
-
-
