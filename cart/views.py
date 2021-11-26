829e94ad0d1b001999dfd42395c38755c5f4f7b0
MarcJPH <marchuntzinger@hotmail.com>
2021-11-26 23:49:21 +0000

    Resolved issue with remove link in cart page

--- cart/views.py
+++ cart/views.py
@@ -35,13 +35,10 @@ def add_to_cart(request, item_id):
 def remove_from_cart(request, item_id):
     """Remove the item from the shopping cart"""
 
-    try:
-        cart.pop(item_id)
-        messages.success(request, f'Removed {product.name} from the cart.')
+    product = get_object_or_404(Product, pk=item_id)
+    cart = request.session.get('cart', {})
+    cart.pop(item_id)
+    messages.success(request, f'Removed {product.name} from the cart.')
 
-        request.session['cart'] = cart
-        return HttpResponse(status=200)
-
-    except Exception as e:
-        messages.error(request, f'Error removing item: {e}.')
-        return HttpResponse(status=500)
+    request.session['cart'] = cart
+    return HttpResponse(status=200)
