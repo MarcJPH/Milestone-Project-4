00d606d920de703ef418123a16c5199ce4287f3d
MarcJPH <marchuntzinger@hotmail.com>
2021-11-28 20:12:16 +0000

    Removed secret key and set debug

--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-53hebbupnldc(umd85m)j-m@#n!#rp5k(1gyxq@4^#tf%dlqw5'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['fitness-app-milestone-project4.herokuapp.com', 'localhost']
 
